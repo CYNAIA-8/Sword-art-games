65a5027afe6bc5bb1a686a0382c5d6936778a8b6
Cynaia Bermudez <106121969+CYNAIA-8@users.noreply.github.com>
2022-07-18 18:33:42 -0400

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// We check directory relative to the file
-import { App } from "./components/App";
+//we check directory relative to the file
 import { ChakraProvider } from "@chakra-ui/react";
+import { App } from "./components/App";
 
-// Creates a root elements in the DOM
-// React DOM library is used to interact with the DOM
+//Creates a root element in the DOM
+//React DOM library is used to interact with the DOM
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <App/>
     </ChakraProvider>
   </React.StrictMode>
 );
